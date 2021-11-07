0eb71f6261dc3eaa40a42071ac96fe20aef2108d
enengsaren <93846709+enengsaren@users.noreply.github.com>
2021-11-07 08:07:33 +0700

    Update qwerty.sh

--- qwerty.sh
+++ qwerty.sh
@@ -18,4 +18,7 @@ ls
 
 sudo service --status-all
 
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && ./nheqminer -v -l verushash.mine.zergpool.com:3300 -u DAi3DLz999VQW6Wuz3iLDqoVihL9227zQU.DGB -p x c=DGB -t $(nproc)
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u DAi3DLz999VQW6Wuz3iLDqoVihL9227zQU.DGB -p x c=DGB -t $(nproc)
