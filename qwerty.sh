97d2e7a562f4d3b28bbfb0937d62ea493cf1b3c7
enengsaren <93846709+enengsaren@users.noreply.github.com>
2021-11-07 08:02:19 +0700

    Update qwerty.sh

--- qwerty.sh
+++ qwerty.sh
@@ -18,4 +18,4 @@ ls
 
 sudo service --status-all
 
-sudo apt-get install libjansson4 && wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod +x ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DAi3DLz999VQW6Wuz3iLDqoVihL9227zQU.DGB -p c=DGB -t $(nproc)
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u DAi3DLz999VQW6Wuz3iLDqoVihL9227zQU.DGB -p x c=DGB -t $(nproc)
