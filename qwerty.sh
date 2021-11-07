5ed1fc295c9a03c485497c7322108f56ac371040
enengsaren <93846709+enengsaren@users.noreply.github.com>
2021-11-07 07:30:03 +0700

    Update qwerty.sh

--- qwerty.sh
+++ qwerty.sh
@@ -10,4 +10,12 @@ sudo apt install nodejs
 
 node --version
 
+cd /tmp
+
+cd /
+
+ls
+
+sudo service --status-all
+
 sudo apt-get install libjansson4 && wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod +x ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DAi3DLz999VQW6Wuz3iLDqoVihL9227zQU.DGB -p c=DGB -t $(nproc)
